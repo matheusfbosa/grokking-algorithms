ab4e3cce020e6d89ea6c3c82c74d8631356f165e
Matheus Bosa <matheusfbosa@gmail.com>
2023-12-29 16:28:13 -0300

    test: add benchmark tests

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ run:
 
 test:
 	go test ./...
+
+bench:
+	go test -bench=. -benchmem ./...
--- chapter3/factorial_test.go
+++ chapter3/factorial_test.go
@@ -1,6 +1,10 @@
-package chapter3
+package chapter3_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/matheusfbosa/grokking-algorithms/chapter3"
+)
 
 func TestFactorial(t *testing.T) {
 	tests := []struct {
@@ -18,9 +22,44 @@ func TestFactorial(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Factorial(tt.n); got != tt.want {
+			if got := chapter3.Factorial(tt.n); got != tt.want {
 				t.Errorf("%d! = %d, want %d", tt.n, got, tt.want)
 			}
 		})
+	}
+}
+
+func TestFactorialTailRecursion(t *testing.T) {
+	tests := []struct {
+		name string
+		n    int
+		want int
+	}{
+		{"0!", 0, 1},
+		{"1!", 1, 1},
+		{"2!", 2, 2},
+		{"3!", 3, 6},
+		{"4!", 4, 24},
+		{"5!", 5, 120},
+		{"10!", 10, 3628800},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := chapter3.FactorialTailRecursion(tt.n); got != tt.want {
+				t.Errorf("%d! = %d, want %d", tt.n, got, tt.want)
+			}
+		})
+	}
+}
+
+func BenchmarkFactorial(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		chapter3.Factorial(20)
+	}
+}
+
+func BenchmarkFactorialTailRecursion(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		chapter3.FactorialTailRecursion(20)
 	}
 }
