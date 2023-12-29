f14377d040fa68fa9e3998903aba71c78d17f06a
Matheus Bosa <matheusfbosa@gmail.com>
2023-12-29 16:27:43 -0300

    feat: add factorial using tail recursion

--- chapter3/factorial.go
+++ chapter3/factorial.go
@@ -1,8 +1,24 @@
 package chapter3
 
+// Factorial calculates the factorial of a given non-negative integer.
 func Factorial(n int) int {
-	if n == 0 || n == 1 {
+	if n <= 1 {
 		return 1
 	}
 	return n * Factorial(n-1)
+}
+
+// FactorialTailRecursion calculates the factorial of a given non-negative integer using a recursive helper function.
+func FactorialTailRecursion(n int) int {
+	return fact(n, 1)
+}
+
+// fact is a helper function for FactorialTailRecursion.
+// It uses recursion to compute the factorial with an accumulator for optimization.
+// Note: Go does not perform true tail call optimization.
+func fact(n, acc int) int {
+	if n <= 1 {
+		return acc
+	}
+	return fact(n-1, acc*n)
 }
